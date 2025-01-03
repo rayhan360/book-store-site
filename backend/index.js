6e32324d3223884ab1698fc499a882b6915ec6ac
rayhan360 <hossainrayhan897@gmail.com>
2025-01-03 23:57:04 +0600

    added cors policy to backend

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,21 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import bookRoute from "./routes/bookRoute.js"
+import cors from 'cors'
 
 const app = express();
 
 // middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling cors policy
+// app.use(cors());
+
+app.use(cors({
+    origin: "http://localhost:5000",
+    methods: ['GET', 'POST', "PUT", "DELETE"],
+    allowedHeaders: ['content-type']
+}))
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
