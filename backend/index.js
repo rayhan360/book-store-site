765409327d2bcdac4427b6945fdef25d5455971c
rayhan360 <hossainrayhan897@gmail.com>
2025-01-03 20:13:29 +0600

    save new books in mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,47 @@
-import express from "express";
+import express, { response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-
+import { Book } from "./model/bookModel.js";
 
 const app = express();
+
+// middleware for parsing request body
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// Route for save a new book
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({message: error.message});
+  }
+});
+
 mongoose
-    .connect(mongoDBURL)
-    .then(()=> {
-        console.log('App connected to database');
-        app.listen(PORT, () => {
-            console.log(`App is listening to port: ${PORT}`);
-          });
-    })
-
-
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
