964218be581160a875f1f4e7cc38205bf2fe4c22
rayhan360 <hossainrayhan897@gmail.com>
2025-01-03 20:23:47 +0600

    Backend - get all books with mongoose

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,21 @@ app.post("/books", async (req, res) => {
   }
 });
 
+// Route for Get all books from database
+app.get('/books', async(req, res) => {
+    try{
+        const books = await Book.find({});
+
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        });
+    }catch(error){
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+})
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
