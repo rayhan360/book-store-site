379fb6855ababb0af1feac274bd53487c873f148
rayhan360 <hossainrayhan897@gmail.com>
2025-01-03 20:30:04 +0600

    Backend- Get one book by id with mongoose

--- backend/index.js
+++ backend/index.js
@@ -48,6 +48,20 @@ app.get('/books', async(req, res) => {
         res.status(500).send({message: error.message});
     }
 })
+// Route for Get one book from database
+app.get('/books/:id', async(req, res) => {
+    try{
+
+        const {id} = req.params;
+
+        const book = await Book.findById(id);
+
+        return res.status(200).json(book);
+    }catch(error){
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+})
 
 mongoose
   .connect(mongoDBURL)
